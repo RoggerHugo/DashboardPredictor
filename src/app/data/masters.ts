a642797ab4dea8811a50303f1c21b31d87a2f213
Puchuri <j.puchuri@cgiar.org>
2025-09-23 23:00:52 -0500

    Actualizando masters

--- src/app/data/masters.ts
+++ src/app/data/masters.ts
@@ -1,19 +1,25 @@
 export const ESTADO_CIVIL = [
-{ id: 1, nombre: 'Soltero' },
-{ id: 2, nombre: 'Casado' },
-{ id: 3, nombre: 'Divorciado' },
+{id: 1, nombre: 'Soltero'},
+{id: 2, nombre: 'Casado'},
+{id: 3, nombre: 'Unión libre'},
+{id: 4, nombre: 'Divorciado'},
+{id: 5, nombre: 'Viudo'},
 ];
 
 
 export const CARRERAS = [
-{ id: 1, nombre: 'Ing. de Sistemas' },
-{ id: 2, nombre: 'Ing. en Animación Digital y Efectos Visuales' },
-{ id: 3, nombre: 'Psicología' },
+{id: 1, nombre: 'Ing. Química'},
+{id: 2, nombre: 'Ing. en Animación Digital y Efectos Visuales'},
+{id: 3, nombre: 'Lic. Administración'},
+{id: 4, nombre: 'Ing. en Sistemas Computacionales'},
+{id: 5, nombre: 'Ing. Mecatrónica'},
 ];
 
 
 export const TURNOS = [
-{ id: 1, nombre: 'Matutino' },
-{ id: 2, nombre: 'Vespertino' },
-{ id: 3, nombre: 'Nocturno' },
+{id: 1, nombre: 'Matutino'},
+{id: 2, nombre: 'Vespertino'},
+{id: 3, nombre: 'Nocturno'},
+{id: 4, nombre: 'Híbrido'},
+{id: 5, nombre: 'En línea'},
 ];
